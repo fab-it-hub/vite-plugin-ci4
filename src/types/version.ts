cd80f98d7da427bac5c476ed13b7708424604e0a
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-01-07 09:37:52 +0530

    feat(src): :bulb: add `version` file for retrived versions

---
+++ src/types/version.ts
@@ -0,0 +1,1 @@
+export declare type GetVersion = () => Promise<string>;
---
+++ src/utils/version.ts
@@ -0,0 +1,21 @@
+import {readJsonFile} from "./read";
+import {highLightError} from "./decorate";
+import type {GetVersion} from "../types/version";
+
+const errorMessage = "version not found, error: ";
+
+export const frameworkVersion: GetVersion = async () => {
+	try {
+		return (await readJsonFile("composer.lock", "codeigniter4/framework")).version;
+	} catch (error) {
+		return highLightError(error, errorMessage);
+	}
+};
+
+export const pluginVersion: GetVersion = async () => {
+	try {
+		return (await readJsonFile("./package.json")).version;
+	} catch (error) {
+		return highLightError(error, errorMessage);
+	}
+};
