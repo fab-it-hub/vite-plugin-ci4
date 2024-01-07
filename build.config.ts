e1babd3a095e8391bbd71c76f91cb2a17328783f
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-01-07 16:41:09 +0530

    feat(src): :sparkles: defined `paths` aliases

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,20 @@
+import path from "path";
 import {defineBuildConfig} from "unbuild";
 
 export default defineBuildConfig({
-	entries: ["./src/index"],
-	outDir: "./dist",
+	entries: ["src/index"],
+	outDir: "dist",
 	externals: ["vite"],
 	declaration: true,
 	clean: true,
 	rollup: {
+		alias: {
+			entries: [
+				{find: "@type", replacement: path.resolve("src/types/")},
+				{find: "@config", replacement: path.resolve("src/config/")},
+				{find: "@utils", replacement: path.resolve("src/utils/")},
+			],
+		},
 		emitCJS: true,
 		inlineDependencies: true,
 		esbuild: {
