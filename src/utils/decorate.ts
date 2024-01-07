249026fd33cb9e24bfb6cdcd57ce0b52fdf66fc3
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-01-07 09:34:29 +0530

    feat(src): :hammer: add `decorate` for highlight terminal message

---
+++ src/utils/decorate.ts
@@ -0,0 +1,20 @@
+import picocolors from "picocolors";
+import type {HighLightError, HighLightVersion} from "../types/decorate";
+
+export const highLightError: HighLightError = (error, tagLine) => {
+	const e = error as Error;
+	let highlight: string = "";
+
+	if (typeof tagLine === "string") highlight += picocolors.dim(tagLine);
+	highlight += picocolors.red(e.message);
+
+	return highlight;
+};
+
+export const highlightVersion: HighLightVersion = (name, version) => {
+	let highlight: string = "  ";
+	highlight += picocolors.white(picocolors.bold(name));
+	highlight += picocolors.green(" v" + version.replace("v", ""));
+
+	return highlight;
+};
