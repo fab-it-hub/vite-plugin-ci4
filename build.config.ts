feaa957b4705471ea690604bb8a4aa8eee70cbe8
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-02-02 09:36:13 +0530

    build(build): :construction_worker: add bun bundler for building library

--- build.config.ts
+++ build.config.ts
@@ -1,24 +1,42 @@
-import path from "path";
-import {defineBuildConfig} from "unbuild";
+import { minify } from "minify";
 
-export default defineBuildConfig({
-	entries: ["src/index"],
-	outDir: "dist",
-	externals: ["vite"],
-	declaration: true,
-	clean: true,
-	rollup: {
-		alias: {
-			entries: [
-				{find: "@type", replacement: path.resolve("src/types/")},
-				{find: "@config", replacement: path.resolve("src/config/")},
-				{find: "@utils", replacement: path.resolve("src/utils/")},
-			],
-		},
-		emitCJS: true,
-		inlineDependencies: true,
-		esbuild: {
-			minify: true,
-		},
-	},
-});
+(async () => {
+	console.log("\n🛠️  Starting building library... \n");
+	const bundler = await Bun.build({
+		minify: true,
+		outdir: "./dist",
+		target: "node",
+		sourcemap: "none",
+		entrypoints: ["./src/index.ts"],
+		external: ["vite", "picocolors", "vite-plugin-full-reload"]
+	});
+
+	if (!bundler.success) {
+		console.error("Error: Something went wrong with building library!");
+		return;
+	}
+
+	const compressHtml = await minify("./index.html", {
+		html: {
+			removeComments: true,
+			removeEmptyAttributes: true,
+			removeRedundantAttributes: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true,
+			minifyCSS: true,
+			minifyJS: true,
+			minifyURLs: true
+		}
+	});
+
+	if (typeof compressHtml !== "string") {
+		console.error("Error: Something went wrong while minify the HTML file.");
+		return;
+	}
+
+	await Bun.write("./dist/index.html", compressHtml, {
+		createPath: true
+	});
+
+	console.log("✅ Library build successfully.");
+})();
