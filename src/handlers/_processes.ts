f4df2652a00a5170ea220f412f6724dc2af001bd
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-06-13 22:07:08 +0530

    refactor(handlers): :art: format process handlers

--- src/handlers/_processes.ts
+++ src/handlers/_processes.ts
@@ -2,13 +2,18 @@ import { appConfig } from '@config/constant'
 import { removeFile } from '@utils/io'
 import { joinPaths } from '@utils/string'
 
-export const _handleExitProcess = () => {
-	const { hotFile, publicDirectory } = appConfig
-
+export const _handleExitProcess = (): void => {
+	process.on('exit', _removeHotFile)
 	process.on('SIGINT', _exitProcess)
-	process.on('SIGHUP', _exitProcess)
 	process.on('SIGTERM', _exitProcess)
-	process.on('exit', () => removeFile(joinPaths(publicDirectory, hotFile)).catch(console.error))
+	process.on('SIGHUP', _exitProcess)
 }
 
 const _exitProcess = (): never => process.exit()
+
+const _removeHotFile = () => {
+	const { hotFile, publicDirectory } = appConfig
+	const hotFilePath = joinPaths(publicDirectory, hotFile)
+
+	removeFile(hotFilePath)
+}
